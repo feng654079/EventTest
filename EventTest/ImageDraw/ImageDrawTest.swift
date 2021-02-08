224c6838f0c1cb0af7d266f8c1e21e6284964747
haoyoumoli <417600039@qq.com>
2021-02-08 17:47:05 +0800

    测试绘制梯度颜色

--- EventTest/ImageDraw/ImageDrawTest.swift
+++ EventTest/ImageDraw/ImageDrawTest.swift
@@ -7,11 +7,13 @@
 //
 
 import Foundation
-
+import CoreGraphics
 
 class ImageDrawViewController: UIViewController {
     
     let imageView = UIImageView()
+    
+    let drawGradientView = DrawGradientView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,10 +24,19 @@ class ImageDrawViewController: UIViewController {
     func setupUI() {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(imageView)
+        
+        drawGradientView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(drawGradientView)
+        
         imageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
         imageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
         imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        drawGradientView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        drawGradientView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        drawGradientView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        drawGradientView.bottomAnchor.constraint(equalTo: imageView.topAnchor,constant: -10.0).isActive = true
     }
 }
 
@@ -75,6 +86,84 @@ extension ImageDrawViewController {
         UIImage.loadImage(name: "bigImage.png", toSize: .init(width: 100.0, height: 100.0)) {
             [weak self]( image) in
             self?.imageView.image = image
+        }
+    }
+}
+
+
+//MARK: -
+extension UIColor{
+    
+    /// 使用hex形式创建color
+    /// - Parameter hexadecimal:rbg格式   e.g #252525 ,0x252525,252525
+    /// - Returns: 返回颜色
+    class func hexadecimalColor(hexadecimal:String,alpha:CGFloat = 1.0) -> UIColor {
+        var colorStr = hexadecimal.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+        if colorStr.hasPrefix("#") {
+            colorStr = String(colorStr.dropFirst())
+        }
+        if colorStr.hasPrefix("0X") {
+            colorStr = String(colorStr.dropFirst(2))
+        }
+        var components = [UInt32]()
+        var index = 0
+        while index < colorStr.count {
+            if
+                let start = colorStr.index(colorStr.startIndex, offsetBy: index,limitedBy: colorStr.endIndex),
+                let end = colorStr.index(start,offsetBy: 2,limitedBy: colorStr.endIndex)
+            {
+                let com = colorStr[start..<end]
+                let scanner = Scanner.init(string: String(com))
+                var value:UInt32 = 0x0
+                if scanner.scanHexInt32(&value) {
+                    components.append(value)
+                }
+            }
+            index += 2
+        }
+        guard components.count == 3 else { return .clear }
+        return UIColor(red: CGFloat(components[0]) / 255.0, green: CGFloat(components[1]) / 255.0, blue: CGFloat(components[2]) / 255.0, alpha: alpha)
+    }
+}
+
+class DrawGradientView: UIView  {
+    
+    override func draw(_ rect: CGRect) {
+        test2(rect)
+    }
+    
+    func test2(_ rect: CGRect) {
+        let locations:[CGFloat] = [0.0,1.0]
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        if let gradient = CGGradient(colorsSpace: colorSpace, colors: [
+            UIColor.hexadecimalColor(hexadecimal: "#ff0000").cgColor,
+            UIColor.hexadecimalColor(hexadecimal: "#00ff00").cgColor
+        ] as CFArray, locations: locations)
+        {
+            let ctx = UIGraphicsGetCurrentContext()
+            ctx?.drawLinearGradient(gradient, start: .zero, end: .init(x:0, y: rect.size.height), options: .drawsAfterEndLocation)
+        }
+    }
+    
+    
+    func test1(_ rect: CGRect) {
+        
+        let ctx = UIGraphicsGetCurrentContext()
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+
+        //red
+        var components1:[CGFloat] = [1.0,0.0,0.0,1.0]
+        let cgColor1 = CGColor.init(colorSpace: colorSpace, components: &components1)
+
+        //green
+        var components2:[CGFloat] = [0.0,1.0,0.5,1.0]
+        let cgColor2 = CGColor.init(colorSpace: colorSpace, components: &components2)
+
+        var locations:[CGFloat] = [0.0,1.0]
+
+        if let gradient = CGGradient.init(colorsSpace: colorSpace, colors: [cgColor1,cgColor2] as CFArray, locations: &locations) {
+            ctx?.drawLinearGradient(gradient, start: .zero, end: .init(x:0, y: rect.size.height), options: .drawsAfterEndLocation)
         }
     }
 }
